86a901c368e3602d64fa54cccf6d52b9b97ef9c8
Piotr Kowalski <piokowam@gmail.com>
2021-07-11 20:07:00 +0200

    file with exchange rates

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 3.7749,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.5085,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.2516,
+    },
+];
