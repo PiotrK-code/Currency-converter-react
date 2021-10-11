5323b552c146fca7153174f352ffdadaa038d5db
Piotr Kowalski <piokowam@gmail.com>
2021-10-11 21:09:13 +0200

    add theme to app

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        black: "#000",
+        emperor: "#555",
+        bulder: "#777",
+        silver: "#ccc",
+        white: "#fff",
+        teal: "#008080",
+    },
+};
